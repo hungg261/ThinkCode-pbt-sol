89e724f3d05ce6c55f89438a0f1db10c46f60d84
hungg261 <phanthanhhung.26012010@gmail.com>
2025-11-19 00:50:31 +0700

    finished (SHOOT2D)

--- ThinkCode-pbt-solutions/SHOOT2D.cpp
+++ ThinkCode-pbt-solutions/SHOOT2D.cpp
@@ -6,8 +6,6 @@ Time (YYYY-MM-DD-hh.mm.ss): 2025-11-18-18.17.42
 #include<bits/stdc++.h>
 using namespace std;
 
-//#define int long long
-
 namespace Utils{
 
 vector<int> values;
@@ -20,9 +18,12 @@ int getVal(const int& val){
     return lower_bound(begin(values), end(values), val) - begin(values) + 1;
 }
 
-int dist(int x1, int y1, int x2, int y2){
-    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+long long pow2(int x){ return 1LL * x * x; }
+
+long long dist(int x1, int y1, int x2, int y2){
+    return pow2(x1 - x2) + pow2(y1 - y2);
 }
+
 
 }
 
@@ -41,97 +42,30 @@ struct Query{
             Utils::values.push_back(x);
         }
     }
+
+    int getXplusY(){ return Utils::getVal(x + y); }
+    int getXminusY(){ return Utils::getVal(x - y); }
+    int getX(){ return Utils::getVal(x); }
+    long long distTo(int _x, int _y){ return Utils::dist(x, y, _x, _y); }
+    long long distTo(const Query& other){ return Utils::dist(x, y, other.x, other.y); }
 };
 
 const int MAXN = 2e5;
 int Q;
 Query queries[MAXN + 5];
 
-//set<int> nodes[MAXN * 4 + 5], lazy[MAXN * 4 + 5], poplazy[MAXN * 4 + 5];
-//void down(const int& id){
-//    for(const int& ele: lazy[id]){
-//        nodes[id << 1].insert(ele);
-//        lazy[id << 1].insert(ele);
-//
-//        nodes[id << 1 | 1].insert(ele);
-//        lazy[id << 1 | 1].insert(ele);
-//    }
-//    lazy[id].clear();
-//}
-//void erase_down(const int& id){
-//    for(const int& ele: poplazy[id]){
-//        nodes[id << 1].erase(ele);
-//        poplazy[id << 1].insert(ele);
-//
-//        nodes[id << 1 | 1].erase(ele);
-//        poplazy[id << 1 | 1].insert(ele);
-//    }
-//    poplazy[id].clear();
-//}
-//
-//void update(int id, int l, int r, int u, int v, int idx){
-//    if(v < l || r < u) return;
-//    if(u <= l && r <= v){
-//        nodes[id].insert(idx);
-//        lazy[id].insert(idx);
-//        return;
-//    }
-//
-//    down(id);
-//    erase_down(id);
-//
-//    int mid = (l + r) >> 1;
-//    update(id << 1, l, mid, u, v, idx);
-//    update(id << 1 | 1, mid + 1, r, u, v, idx);
-//
-//    for(const int& ele: nodes[id << 1]) nodes[id].insert(ele);
-//    for(const int& ele: nodes[id << 1 | 1]) nodes[id].insert(ele);
-//}
-//
-//void pop(int id, int l, int r, int u, int v, int idx){
-//    if(v < l || r < u) return;
-//    if(u <= l && r <= v){
-//        nodes[id].erase(idx);
-//        poplazy[id].insert(idx);
-//        return;
-//    }
-//
-//    down(id);
-//    erase_down(id);
-//
-//    int mid = (l + r) >> 1;
-//    pop(id << 1, l, mid, u, v, idx);
-//    pop(id << 1 | 1, mid + 1, r, u, v, idx);
-//
-//    for(const int& ele: nodes[id << 1]) nodes[id].insert(ele);
-//    for(const int& ele: nodes[id << 1 | 1]) nodes[id].insert(ele);
-//}
-//
-//set<int> get(int id, int l, int r, const int& idx){
-//    if(r < idx || idx < l) return {};
-//    if(l == r){
-//        return nodes[id];
-//    }
-//
-//    down(id);
-//    erase_down(id);
-//
-//    int mid = (l + r) >> 1;
-//    set<int> L = get(id << 1, l, mid, idx), R = get(id << 1 | 1, mid + 1, r, idx);
-//    for(const int& ele: R) L.insert(ele);
-//    return L;
-//}
-
-vector<int> nodes[MAXN * 4 + 5], lazy[MAXN * 4 + 5];
+vector<int> nodes[MAXN * 8 + 5], lazy[MAXN * 8 + 5];
 bool mark[MAXN + 5];
-void down(const int& id){
-    int sz = lazy[id].size();
-    for(int i = 0; i < sz; ++i){
-        nodes[id << 1].push_back(lazy[id][i]);
-        lazy[id << 1].push_back(lazy[id][i]);
 
-        nodes[id << 1 | 1].push_back(lazy[id][i]);
-        lazy[id << 1 | 1].push_back(lazy[id][i]);
+void down(int id){
+    for(int idx: lazy[id]){
+        if(mark[idx]) continue;
+
+        nodes[id << 1].push_back(idx);
+        lazy[id << 1].push_back(idx);
+
+        nodes[id << 1 | 1].push_back(idx);
+        lazy[id << 1 | 1].push_back(idx);
     }
     lazy[id].clear();
 }
@@ -151,22 +85,26 @@ void update(int id, int l, int r, int u, int v, int idx){
     update(id << 1 | 1, mid + 1, r, u, v, idx);
 }
 
-void pop(int idx){
-    mark[idx] = true;
-}
-
-void get(int id, int l, int r, const int& idx, vector<int>& res){
+void get(int id, int l, int r, int idx, vector<int>& res){
     if(r < idx || idx < l) return;
 
-    down(id);
-    for(int ele: nodes[id]){
-        if(!mark[ele]) res.push_back(ele);
+    int sz = nodes[id].size();
+    for(int i = 0; i < sz; ++i){
+        while(i < sz && mark[nodes[id][i]]){
+            swap(nodes[id][i], nodes[id][sz - 1]);
+            nodes[id].pop_back();
+            --sz;
+        }
+        if(i < sz && !mark[nodes[id][i]]){
+            res.push_back(nodes[id][i]);
+        }
     }
 
     if(l == r){
         return;
     }
 
+    down(id);
 
     int mid = (l + r) >> 1;
     if(idx <= mid) get(id << 1, l, mid, idx, res);
@@ -187,18 +125,16 @@ signed main(){
     const int MAXVAL = Utils::values.size() + 1;
     for(int i = 1; i <= Q; ++i){
         if(queries[i].type == 1){
-            update(1, 1, MAXVAL, Utils::getVal(queries[i].x - queries[i].y), Utils::getVal(queries[i].x + queries[i].y), i);
+            update(1, 1, MAXVAL, queries[i].getXminusY(), queries[i].getXplusY(), i);
         }
         else{
-//            for(int ele: get(1, 1, MAXVAL, Utils::getVal(queries[i].x))){
-//                cerr << "\t" << ele << '\n';
-//            }
             vector<int> res;
-            get(1, 1, MAXVAL, Utils::getVal(queries[i].x), res);
+            get(1, 1, MAXVAL, queries[i].getX(), res);
             for(int ele: res){
-                if(Utils::dist(queries[i].x, queries[i].y, queries[ele].x, queries[ele].y) < queries[ele].y * queries[ele].y){
+                if(mark[ele]) continue;
+                if(queries[i].distTo(queries[ele]) < Utils::pow2(queries[ele].y)){
                     cout << ele << '\n';
-                    pop(ele);
+                    mark[ele] = true;
                     goto passed;
                 }
             }
@@ -210,25 +146,3 @@ signed main(){
 
     return 0;
 }
-
-/*
-8
-1 0 12
-2 -11 22
-1 24 10
-1 12 3
-2 12 12
-2 16 14
-1 28 15
-2 17 13
-
-8
-1 0 12
-2 -11 22
-1 24 10
-1 12 3
-2 12 12
-2 16 14
-1 28 15
-2 12 12
-*/
