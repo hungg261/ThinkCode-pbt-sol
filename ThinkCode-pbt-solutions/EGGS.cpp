a56e2482fcaae3385d85e73e2f6ce3858822579c
hungg261 <phanthanhhung.26012010@gmail.com>
2025-11-22 23:33:49 +0700

    unfinished

---
+++ ThinkCode-pbt-solutions/EGGS.cpp
@@ -0,0 +1,72 @@
+/******************************************************************************
+Link: EGGS
+Code: EGGS
+Time (YYYY-MM-DD-hh.mm.ss): 2025-11-22-22.29.32
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+const int OFFSET = 4240;
+int dp[53][2][2][2][2][2][8485];
+int numL[53], numR[53];
+int Try(int idx, int zero1, int zero2, int smaller1, int larger1, int smaller2, int larger2, int less, int diff){
+    if(idx < 0) return less && diff > OFFSET;
+
+    int& memo = dp[idx][smaller1][larger1][smaller2][larger2][less][diff];
+    if(memo != -1) return memo;
+
+    int bot1 = larger1 ? 0 : numL[idx];
+    int bot2 = larger2 ? 0 : numR[idx];
+
+    int lim1 = smaller1 ? 80 : numL[idx];
+    int lim2 = smaller2 ? 80 : numR[idx];
+
+    memo = 0;
+    for(int digit1 = bot1; digit1 <= lim1; ++digit1){
+        int base = max(bot2, (less ? 0 : digit1));
+        for(int digit2 = base; digit2 <= lim2; ++digit2){
+            memo += Try(idx - 1,
+                        (zero1 && digit1 == 0), (zero2 && digit2 == 0),
+                        (smaller1 || (digit1 < lim1)), (larger1 || (digit1 > bot1)),
+                        (smaller2 || (digit2 < lim2)), (larger2 || (digit2 > bot2)),
+                        (less || (digit1 < digit2)),
+                        diff + (zero1 || zero2 ? 0 : (digit1 - digit2)));
+        }
+    }
+
+    return memo;
+}
+
+int solve(int L, int R){
+    int lenL = 0, lenR = 0;
+
+    int tmpL = L, tmpR = R;
+    while(tmpL > 0){
+        numL[lenL++] = tmpL % 81;
+        tmpL /= 81;
+    }
+
+    while(tmpR > 0){
+        numR[lenR++] = tmpR % 81;
+        tmpR /= 81;
+    }
+
+    int len = max(lenL, lenR);
+
+    memset(dp, -1, sizeof dp);
+    return Try(len - 1, 1, 1, 0, 0, 0, 0, 0, OFFSET);
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    //freopen("EGGS.INP","r",stdin);
+    //freopen("EGGS.OUT","w",stdout);
+    int L, R;
+    cin >> L >> R;
+
+    cout << solve(L, R) << '\n';
+
+    return 0;
+}
