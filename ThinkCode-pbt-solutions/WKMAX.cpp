5b05726691bab89400f471487e22a93450722efa
hungg261 <phanthanhhung.26012010@gmail.com>
2025-12-02 21:51:31 +0700

    added hints

--- ThinkCode-pbt-solutions/WKMAX.cpp
+++ ThinkCode-pbt-solutions/WKMAX.cpp
@@ -1,150 +1,106 @@
-// Khanh Minh's: https://ideone.com/vjGr73
-
-#include <bits/stdc++.h>
-#define taskname "main"
+/******************************************************************************
+Link: WKMAX
+Code: WKMAX
+Time (YYYY-MM-DD-hh.mm.ss): 2025-11-27-13.01.02
+*******************************************************************************/
+#include<bits/stdc++.h>
 using namespace std;
-const int 
-    maxN = 1e5,
-    mod = 1e9 + 7;
-int n, k,
-    tro_l[maxN + 5],
-    tro_r[maxN + 5];
-long long 
-    res_s,
-    p[maxN + 1];
-struct HasI{
-    long long x; int i;
-    void read(int _i){
-        cin>>x;
-        i = _i;
+
+struct FenwickTree{
+    int n;
+    vector<int> BIT;
+
+    FenwickTree() = default;
+    FenwickTree(int sz): n(sz){
+        BIT.resize(sz + 1, 0);
     }
-    void print(){
-        cout<<x<<' '<<i<<'\n';
+
+    void update(int idx, int val){
+        for(int i = idx; i <= n; i += i & -i){
+            BIT[i] += val;
+        }
     }
-    bool operator < (const HasI &other)const{
-        return x > other.x || x == other.x && i < other.i;
-    }
-} a[maxN + 5], org_a[maxN + 5];
-struct FenTree{
-    long long bit[maxN + 5];
-private:
-    long long get(int u){
-        long long res = 0;
-        for (int i = u; i > 0; i -= i & -i)
-            res = (res + bit[i]) % mod;
+
+    int get(int idx){
+        int res = 0;
+        for(int i = idx; i > 0; i -= i & -i){
+            res += BIT[i];
+        }
         return res;
     }
-public:
-    void clear(){
-        memset(bit, 0, sizeof bit);
-    }
-    void upd(int u, int val){
-        for (int i = u; i <= n; i += i & -i)
-            bit[i] = (bit[i] + val) % mod;
-    }
-    long long get_r(int u, int v){
-        return (get(v) - get(u - 1) + mod) % mod;
-    }
-} ftcnt, ftsum;
-queue <HasI> q;
-int bs_l(int idx){
-    int lo = 1, hi = idx, res = 0;
-    while (lo <= hi){
-        int mid = (lo + hi) >> 1;
-        if (ftcnt.get_r(mid, idx)){
-            lo = mid + 1;
-            res = max(res, mid);
+    int get(int L, int R){ return get(R) - get(L - 1); }
+
+    int jumpLeft(int idx, int steps){
+        int target = get(idx) - steps + 1;
+        int l = 1, r = idx, res = -1;
+        while(l <= r){
+            int mid = (l + r) >> 1, value = get(mid);
+            if(value >= target){
+                if(value == target) res = mid;
+                r = mid - 1;
+            }
+            else l = mid + 1;
         }
-        else hi = mid - 1;
+
+        return res;
     }
-    return res;
-}
-int bs_r(int idx){
-    int lo = idx, hi = n, res = n + 1;
-    while (lo <= hi){
-        int mid = (lo + hi) >> 1;
-        if (ftcnt.get_r(idx, mid)){
-            hi = mid - 1;
-            res = min(res, mid);
+
+    int jumpRight(int idx, int steps){
+        int target = get(idx) + steps - 1;
+        int l = idx, r = n, res = -1;
+        while(l <= r){
+            int mid = (l + r) >> 1, value = get(mid);
+            if(value >= target){
+                if(value == target) res = mid;
+                r = mid - 1;
+            }
+            else l = mid + 1;
         }
-        else lo = mid + 1;
+
+        return res;
     }
-    return res;
-}
-void init(){
-    cin>>n>>k; 
-    for (int i = 1; i <= n; i++){
-        a[i].read(i);
-        org_a[i] = a[i];
+};
+
+const int MAXN = 1e5, MAXK = 100;
+int n, k, arr[MAXN + 5];
+
+pair<int, int> temp[MAXN + 5];
+void trau(){
+    for(int i = 1; i <= n; ++i){
+        temp[i] = {arr[i], i};
     }
-    sort(a + 1, a + n + 1);
-    ftcnt.clear(); ftsum.clear();
-    for (int i = 1; i <= n; i++){
-        int tmp_l = bs_l(a[i].i),
-            tmp_r = bs_r(a[i].i);
-        tro_l[a[i].i] = tmp_l;
-        tro_r[a[i].i] = tmp_r;
-        ftcnt.upd(a[i].i, 1);
-    }
-}
-void f_slide(int &cnt, int &tmp, int tro[], int step, int lim){
-    for (int i = 1; i <= step; i++){
-        if (tro[tmp] != lim){
-            cnt++;
-            tmp = tro[tmp];
+    sort(temp + 1, temp + n + 1, greater<>());
+
+    FenwickTree mark(n);
+    int ans = 0;
+    for(int i = 1; i <= n; ++i){
+        int cur, idx; tie(cur, idx) = temp[i];
+
+        mark.update(idx, 1);
+        for(int x = 0; x <= k; ++x){
+            int y = k - x;
+
+            int L = mark.jumpLeft(idx, x); if(L == -1) continue;
+            int R = mark.jumpRight(idx, y); if(R == -1) continue;
+
+            ans += (idx - L - 1) * (R - idx - 1) * cur;
         }
     }
+
+    cout << ans << '\n';
 }
-void upd_val(int idx){
-    tro_r[tro_l[a[idx].i]] = a[idx].i;
-    tro_l[tro_r[a[idx].i]] = a[idx].i;
-    ftsum.upd(a[idx].i, a[idx].x);
-    q.push(a[idx]);
-}
-void slide_1(int &tmp_l, int &tmp_r, long long &tmp_s){
-    tmp_s -= org_a[tmp_l].x;
-    tmp_l = tro_r[tmp_l];
-    tmp_r = tro_r[tmp_r];
-    tmp_s += org_a[tmp_r].x;
-}
-int xet(HasI org){
-    long long res = 0;
-    int tmp_l = org.i, cnt_l = 1,
-        tmp_r = org.i, cnt_r = 1;
-    f_slide(cnt_l, tmp_l, tro_l, k - 1, 0);
-    f_slide(cnt_r, tmp_r, tro_r, k - cnt_l, n + 1);
-    long long tmp_s = ftsum.get_r(tmp_l, tmp_r);
-    if (cnt_l + cnt_r - 1 == k){
-        while (tmp_l <= org.i && tmp_r <= n){
-            res = (res + tmp_s * ((tmp_l - tro_l[tmp_l] + mod) % mod) % mod * ((tro_r[tmp_r] - tmp_r + mod) % mod)) % mod;
-            slide_1(tmp_l, tmp_r, tmp_s);
-        }
+
+signed main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    //freopen("WKMAX.INP","r",stdin);
+    //freopen("WKMAX.OUT","w",stdout);
+
+    cin >> n >> k;
+    for(int i = 1; i <= n; ++i){
+        cin >> arr[i];
     }
-    return res;
-}
-void f_big(){
-    for (int i = 1; i <= n; i++){
-        upd_val(i);
-        res_s = (res_s + xet(a[i])) % mod;
-    }
-}
-void f_small(){
-    ftsum.clear();
-    for (int i = 1; i <= n; i++){
-        p[i] = (p[i - 1] + org_a[i].x) % mod;
-        ftsum.upd(i, p[i]);
-    }
-    for (int i = 1; i < k; i++)
-        res_s = (res_s + (i * p[i] % mod - ftsum.get_r(1, i - 1) + mod) % mod) % mod;
-    for (int i = k; i <= n; i++)
-        res_s = (res_s + ((k - 1) * p[i] % mod - ftsum.get_r(i - k + 1, i - 1) + mod) % mod) % mod;
-    cout<<res_s;
-}
-signed main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    #ifndef ONLINE_JUDGE
-        freopen(taskname".inp","r", stdin);
-        freopen(taskname".out","w",stdout);
-    #endif
-    init(); f_big(); f_small();
+
+    trau();
+
+    return 0;
 }
